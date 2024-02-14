7a506f5851e8047d8c252b739aa33a9c37481876
robert.downer <Robert.Downer@fdmgroup.com>
2024-02-14 16:58:27 +0000

    Added cros origins

--- src/main/java/com/example/EventManagement/Controllers/UserController.java
+++ src/main/java/com/example/EventManagement/Controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 	private IUserService userService;
